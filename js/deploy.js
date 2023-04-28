83b186afb6b4b43cc23714a7b9f5c0c51a835da0
Joaquin Calle Aranzolo <joaquincalleunlp@hotmail.com>
2023-04-28 12:19:57 -0300

    Fix Linter's error

--- js/deploy.js
+++ js/deploy.js
@@ -3,35 +3,35 @@ const details = [
     img: './img/mobile/gustavo-cerati.png',
     title: 'Gustavo Cerati',
     sub_title: 'Singer',
-    paragraph: 'Author from all of his songs, known all over the world, one of the greatest'
+    paragraph: 'Author from all of his songs, known all over the world, one of the greatest',
   },
 
   {
     img: './img/mobile/gustavo-cerati.png',
     title: 'Zeta Bosio',
     sub_title: 'Guitar Player',
-    paragraph: 'The right hand of Gustavo in the stage. Born in San Fernando, Buenos Aires.'
+    paragraph: 'The right hand of Gustavo in the stage. Born in San Fernando, Buenos Aires.',
   },
 
   {
     img: './img/mobile/gustavo-cerati.png',
     title: 'Charly Alberti',
     sub_title: 'Drummer',
-    paragraph: 'Drummer of one the greatest bands ever. Born in Buenos Aires.'
+    paragraph: 'Drummer of one the greatest bands ever. Born in Buenos Aires.',
   },
 
   {
     img: './img/mobile/gustavo-cerati.png',
     title: 'Charly Garcia',
     sub_title: 'Piano/Singer',
-    paragraph: 'The Famous -Cuna del Rock-, the creator of the Rock Nacional.'
+    paragraph: 'The Famous -Cuna del Rock-, the creator of the Rock Nacional.',
   },
 
   {
     img: './img/mobile/gustavo-cerati.png',
     title: 'Luis Alberto Spinnetta',
     sub_title: 'Guitar/Singer',
-    paragraph: 'Know for his complexity in the guitar, angelical voice, the impulse if the Rock Nacional'
+    paragraph: 'Know for his complexity in the guitar, angelical voice, the impulse if the Rock Nacional',
   },
 
 ];
@@ -54,5 +54,5 @@ for (let i = 0; i < details.length; i += 1) {
     <h5 class="members-h5">${details[i].sub_title}</h5>
     <p class="members-p">${details[i].paragraph}</p>
   </div>
- </div>`
+ </div>`;
 }
